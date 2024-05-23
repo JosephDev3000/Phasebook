b6e611ac076ec0f89b6ae1fe6c21140777fd2702
JosephDev3000 <jvbriones1999@gmail.com>
2024-05-23 11:30:25 +0800

    user_filter

--- phasebook/search.py
+++ phasebook/search.py
@@ -25,6 +25,50 @@ def search_users(args):
         a list of users that match the search parameters
     """
 
-    # Implement search here!
+    id = args.get("id")
+    name = args.get("name", "").lower()
+    age = args.get("age")
+    occupation = args.get("occupation", "").lower()
 
-    return USERS
+    result = []
+
+    match_priority = []
+
+    if id:
+        
+        user = next((user for user in USERS if user["id"] == id), None)
+        if user:
+            result.append(user)
+            match_priority.append((user, 'id'))
+
+    for user in USERS:
+        if user in result:
+            continue  
+
+        matched_by = None
+        if name and name in user["name"].lower():
+            matched_by = 'name'
+        if age and int(age) - 1 <= user["age"] <= int(age) + 1:
+            matched_by = 'age'
+        if occupation and occupation in user["occupation"].lower():
+            matched_by = 'occupation'
+
+        if matched_by:
+            result.append(user)
+            match_priority.append((user, matched_by))
+
+    
+    def get_priority(match):
+        user, criteria = match
+        priority = {
+            'id': 0,
+            'name': 1,
+            'age': 2,
+            'occupation': 3
+        }
+        return priority[criteria]
+
+    sort_result = sorted(match_priority, key=get_priority)
+    sort_users = [match[0] for match in sort_result]
+
+    return sort_users
